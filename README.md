e98ca9f833ed01b561e815ff31fa7e9576504d65
Mohammed Sufyan <Mohammed Sufyan>
2016-09-20 08:06:27 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # -PHP-Bitly-Library
 Simple function to get short url
+
+* Add your access_token in config.json file.
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+  require_once('bitly.php');
+  /* Example How to get ShortUrl */
+  $url = getShortUrl('http://sufyan.co.in/');
+  echo "ShortUrl: ".$url;
+?>
